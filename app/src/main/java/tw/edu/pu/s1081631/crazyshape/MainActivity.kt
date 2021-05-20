63cd44a5cdf874638cf8bc8bbdada2a429c00193
吳曜宇 <s1081631@gm.pu.edu.tw>
2021-05-20 22:12:42 +0800

    2021上機考v0_(吳曜宇)

---
+++ app/src/main/java/tw/edu/pu/s1081631/crazyshape/MainActivity.kt
@@ -0,0 +1,32 @@
+package tw.edu.pu.s1081631.crazyshape
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_game.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(this, "作者： (吳曜宇)", Toast.LENGTH_SHORT).show()
+
+        imageButton.setOnLongClickListener{
+            var it = Intent(this, GameActivity::class.java);
+            startActivity(it)
+            true
+        }
+
+    }
+
+
+
+}
